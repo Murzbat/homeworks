687ba34624e702d9317628b408d9b74143411d7c
Murad Bayramov <murzilkaa68@gmail.com>
2018-08-15 18:56:55 +0300

    Это наш первый комит

---
+++ Lesson 1/script.js
@@ -0,0 +1,21 @@
+var budget = +prompt("Ваш бюджет на месяц?");
+//console.log(typeof(budget));
+if (typeof(budget)==!number)
+
+var name = prompt("Название вашего магазина?");
+
+var mainList={
+	ShopBudget: budget,
+	ShopName: name,
+	ShopGoods: [],
+	emploeyers: {},
+	open: true
+}
+
+for (var i=0; i<3; i++) {
+	mainList.ShopGoods[i]=prompt("Какой тип товаров будем продавать?");
+}
+
+alert("Ваш бюджет на один день составляет "+budget/30);
+//console.log(typeof(budget/30));
+//console.log(mainList.ShopGoods);
