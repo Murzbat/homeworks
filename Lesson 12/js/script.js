a044a7131d4b29bbd555b055b4b322a261e5236f
Murad Bayramov <murzilkaa68@gmail.com>
2018-09-02 20:34:27 +0300

    19й коммит

--- Lesson 12/js/script.js
+++ Lesson 12/js/script.js
@@ -283,9 +283,7 @@ window.addEventListener('DOMContentLoaded', function(){
 				//console.log(this.value);
 
 				total = (daysSum + personsSum)*4000;
-				persons.onkeyup = function (input){
-      				  return this.value = this.value.replace(/[^\d]/g, '');
-				};
+				
 				if (persons.value == '' || restDays.value == '' || persons.value == 0 || restDays.value == 0)  {
   					  totalValue.innerHTML = '0';
 				}else {
@@ -293,19 +291,22 @@ window.addEventListener('DOMContentLoaded', function(){
 				}
 
 			});
+			persons.onkeyup = function (input){
+      				  return this.value = this.value.replace(/[^\d]/g, '');
+				};
 
 			restDays.addEventListener('change', function(){
 				daysSum = +this.value;
 				total = (daysSum + personsSum)*4000;
-				restDays.onkeyup = function (input){
-      				  return this.value = this.value.replace(/[^\d]/g, '');
-				};
 				if (persons.value == '' || restDays.value == '' || persons.value == 0 || restDays.value == 0)  {
   					  totalValue.innerHTML = '0';
 				}else {
    					 totalValue.innerHTML = total;
 				}
-			})
+			});
+			restDays.onkeyup = function (input){
+      				  return this.value = this.value.replace(/[^\d]/g, '');
+				};
 
 			place.addEventListener('change', function(){
 				if (restDays.value == '' || persons.value =='' || ((+persons.value ^ 0)) !== +persons.value || ((+restDays.value ^ 0)) !== +restDays.value || daysSum == 0 || personsSum == 0){
