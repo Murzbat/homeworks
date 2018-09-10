2f6cdbfc9fc7f6e3b375fb74318302eab601009f
Murad Bayramov <murzilkaa68@gmail.com>
2018-09-10 17:23:57 +0300

    2й коммит

--- Project/js/script.js
+++ Project/js/script.js
@@ -30,7 +30,6 @@ window.addEventListener('DOMContentLoaded', function(){
 			}
 		})
 
-		console.log(phone);
 
 		modalForm2.addEventListener('click', function(){
 			
@@ -51,24 +50,98 @@ window.addEventListener('DOMContentLoaded', function(){
 
 			})
 			
-			// closing the modal window by clicking on the X
 			
-			popup_close[i].addEventListener('click', function(){
-				if ( i == 1 ) {
-
-					modalForm1.style.display = 'none';
-					document.body.style.overflow = '';
-				
-				} else {
-					modalForm2.style.display = 'none';
-					document.body.style.overflow = '';
-				}
-			
-			})
 
 		}	
 
+// closing the modal window by clicking on the X
+			
+			popup_close[1].addEventListener('click', function(){
+					modalForm1.style.display = 'none';
+					document.body.style.overflow = '';
+			})
+				
+			popup_close[0].addEventListener('click', function(){
 
 
+					modalForm2.style.display = 'none';
+					document.body.style.overflow = '';
+			
+			})
+
+
+		//Moda Forms
+
+		let message = new Object();
+		message.loading = "Загрузка...";
+		message.succes = "Спасибо! Скоро мы с вами свяжемся";
+		message.failure = "Что-то пошло не так...";
+
+			let form = document.getElementsByClassName('form'),
+				input = [];
+			statusMessage = document.createElement('div');
+		statusMessage.classList.add('status');
+		
+		for (let i = 0; i < form.length; i++){
+			input[i] = [];
+			for (let j = 0; j < 2; j++){	
+				input[i] = form[i].getElementsByTagName('input');
+			}	
+		}
+
+
+
+		//цикл для того,чтобы обращаться к каждому модальному окну
+		for (let i = 0; i < form.length; i++){
+			//запрет на ввод всего кроме цифр
+			input[i][1].onkeyup = function (input){
+      				  return this.value = this.value.replace(/[^\d]/g, '');
+			};
+		
+			form[i].addEventListener('submit', function(event){
+				event.preventDefault();
+				form[i].appendChild(statusMessage);
+
+				//Ajax
+				
+				let request = new XMLHttpRequest();
+				
+				request.open("POST",'server.php');
+
+				request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+				let formData = new FormData(form[i]);
+				
+				request.send(formData);
+
+				request.onreadystatechange = function(){
+					if (request.readyState < 4) {
+						
+						statusMessage.innerHTML = message.loading;
+					
+					} else if ( request.readyState === 4) {
+					
+						if (request.status === 200 && request.status < 300) {
+							
+							statusMessage.innerHTML = message.succes;
+						
+						} else {
+						
+							statusMessage.innerHtml = message.failure;
+						
+						}
+					}
+				}			
+
+			
+				for (let j = 0; j < 2; j++) {
+					
+					input[i][j].value = '';
+					//Очищаем поля ввода
+				}
+
+			})
+		}
+		
 })
 
